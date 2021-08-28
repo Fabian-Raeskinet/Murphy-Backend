3ba5806e21df8296f42685c5c43de67031c665fa
Raeskinet <fabian.raeskinet@hotmail.com>
2021-08-28 16:17:39 +0200

    Role Model

---
+++ src/Domain/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public class Role
+    {
+        public string RoleId { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Position { get; set; }
+        public int MemberRoleCount { get; set; }
+    }
+}
